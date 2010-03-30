f2c6416e7459e864310430ca8a59a97c21a5ec9d
Kyle McMartin <kyle@phobos.i.jkkm.org>
2010-03-30 00:04:29 -0400

    die-floppy-die
    Kill the floppy.ko pnp modalias. We were surviving just fine without
    autoloading floppy drivers, tyvm.
    
    Please feel free to register all complaints in the wastepaper bin.
    
    Bugzilla: N/A
    Upstream-status: Fedora mustard

--- drivers/block/floppy.c
+++ drivers/block/floppy.c
@@ -4963,8 +4963,7 @@ static const struct pnp_device_id floppy_pnpids[] = {
 	{"PNP0700", 0},
 	{}
 };
-
-MODULE_DEVICE_TABLE(pnp, floppy_pnpids);
+/* MODULE_DEVICE_TABLE(pnp, floppy_pnpids); */
 
 #else
 
